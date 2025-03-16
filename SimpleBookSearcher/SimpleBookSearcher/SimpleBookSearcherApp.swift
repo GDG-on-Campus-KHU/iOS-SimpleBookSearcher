0c2915363a1d7708fc8881fd3ce1b50baa9968f0
binschocolate <hisbulletp@gmail.com>
2025-03-16 17:09:51 +0900

    initial project

---
+++ SimpleBookSearcher/SimpleBookSearcher/SimpleBookSearcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleBookSearcherApp.swift
+//  SimpleBookSearcher
+//
+//  Created by 박현빈 on 3/16/25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleBookSearcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
